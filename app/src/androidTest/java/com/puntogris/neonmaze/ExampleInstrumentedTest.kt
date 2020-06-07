6c21be2add9f9eacb519002fa354bc29a473199f
puntogris <rosariodota3@gmail.com>
2020-06-07 19:56:46 -0300

    added screenshots

--- app/src/androidTest/java/com/puntogris/neonmaze/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/puntogris/neonmaze/ExampleInstrumentedTest.kt
@@ -21,4 +21,5 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.puntogris.neonmaze", appContext.packageName)
     }
+
 }
